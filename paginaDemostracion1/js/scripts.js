f288d1318ca58b8e4958f7d0d04084c94feb0954
elbrayanpvp <elbrayanpaicob@gmail.com>
2024-09-02 19:47:33 -0500

    Delete paginaDemostracion1 directory

--- paginaDemostracion1/js/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const cards = document.querySelectorAll('.card');
-
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            alert(`Has seleccionado ${card.querySelector('h3').innerText}`);
-        });
-    });
-});
